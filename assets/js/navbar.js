2649ec26550ec7f1cdd10af796b9f97e2dc89bec
OutdatedGuy <74326345+OutdatedGuy@users.noreply.github.com>
2021-06-06 03:23:51 +0530

    Bootstrap Design Website

---
+++ assets/js/navbar.js
@@ -0,0 +1,14 @@
+$(".navbar-nav li a").click(function (event) {
+    var toggle = $(".navbar-collapse").hasClass("show");
+    if (toggle) {
+      $(".navbar-toggler").click();
+    }
+  });
+
+  $('.navbar-nav > li > a').on('click', function() {
+    $('.navbar-collapse').collapse('hide');
+});
+
+$('.navbar-nav>li>a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+});
