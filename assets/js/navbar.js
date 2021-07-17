2c2d1a6f7b900e3f1f77d5f5a28674add25029cc
OutdatedGuy <74326345+OutdatedGuy@users.noreply.github.com>
2021-07-17 20:33:15 +0530

    Added auto close navbar when clicked outside

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,14 +1,21 @@
 $(".navbar-nav li a").click(function (event) {
-    var toggle = $(".navbar-collapse").hasClass("show");
-    if (toggle) {
-      $(".navbar-toggler").click();
-    }
-  });
-
-  $('.navbar-nav > li > a').on('click', function() {
-    $('.navbar-collapse').collapse('hide');
+  var toggle = $(".navbar-collapse").hasClass("show");
+  if (toggle) {
+    $(".navbar-toggler").click();
+  }
 });
 
-$('.navbar-nav>li>a').on('click', function(){
-    $('.navbar-collapse').collapse('hide');
+$(".navbar-nav > li > a").on("click", function () {
+  $(".navbar-collapse").collapse("hide");
+});
+
+$(".navbar-nav>li>a").on("click", function () {
+  $(".navbar-collapse").collapse("hide");
+});
+
+// function to hide the navbar when clicking on the page
+$(document).on("click", function (e) {
+  if ($(e.target).closest(".navbar").length === 0) {
+    $(".navbar-collapse").collapse("hide");
+  }
 });
